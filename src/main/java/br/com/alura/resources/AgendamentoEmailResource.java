3dc896a829d57fd44f6484b8424cdf0cc5bea64d
Daniel Bou <danielbou@outlook.com.br>
2020-07-05 22:34:16 -0300

    Gerando log da aplicação

--- src/main/java/br/com/alura/resources/AgendamentoEmailResource.java
+++ src/main/java/br/com/alura/resources/AgendamentoEmailResource.java
@@ -1,8 +1,11 @@
 package br.com.alura.resources;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import javax.ejb.EJBException;
 import javax.inject.Inject;
+import javax.validation.ConstraintViolationException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -16,6 +19,8 @@ import br.com.alura.entity.AgendamentoEmail;
 
 @Path("/agendamentoemail")
 public class AgendamentoEmailResource {
+	
+	private static Logger logger = Logger.getLogger(AgendamentoEmailResource.class.getName());
 	
 	@Inject
 	private AgendamentoEmailBusiness agendamentoEmailBusiness;
@@ -33,7 +38,17 @@ public class AgendamentoEmailResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response salvarAgendamentoEmail(AgendamentoEmail agendamentoEmail) {
 		
-		agendamentoEmailBusiness.salvarAgendamentoEmail(agendamentoEmail);
+		try {
+			agendamentoEmailBusiness.salvarAgendamentoEmail(agendamentoEmail);
+		}
+		catch (EJBException e) {
+			if (e.getCause() instanceof ConstraintViolationException) {
+				logger.info(e.getMessage());
+			} else {
+				logger.severe(e.getMessage());
+			}
+			throw e;
+		}
 		
 		return Response
 				.status(201)
